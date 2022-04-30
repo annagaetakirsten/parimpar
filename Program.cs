6edfe958bc47c527a12a54dd949ff6c93854e1bc
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-04-29 21:31:36 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿int numeroDigitado;
+Console.Write ("digite um numero ");
+numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+bool numeroEhPar = (numeroDigitado % 2) == 0;
+
+if (numeroEhPar)
+
+{
+  Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine($"{numeroDigitado} é par ");
+}
+
+ else
+ {
+     Console.ForegroundColor = ConsoleColor.Blue;
+     Console.WriteLine ($"{numeroDigitado} é ímpar");
+ }
+
+ Console.ResetColor();
